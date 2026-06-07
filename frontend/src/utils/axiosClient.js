cb08f4fd8a471797e208cc92d352b9ec03a293ca
shivamgupta78 <shivamgupta2154@gmail.com>
2026-06-07 19:23:50 +0530

    Full Stack Catcode Ready

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL:'http://https://catcode-server.vercel.app',
+    baseURL:'https://catcode-server.vercel.app',
     withCredentials:true,
     headers:{
         'Content-Type':'application/json'
