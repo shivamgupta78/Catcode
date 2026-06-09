d0eed574a8a1aea7a0d58ee67e0c70933511afc3
shivamgupta78 <shivamgupta2154@gmail.com>
2026-06-09 13:25:41 +0530

    Middleware error resolved

--- Backend/src/middlewares/userMiddlewares.js
+++ Backend/src/middlewares/userMiddlewares.js
@@ -5,7 +5,7 @@ const User = require('../models/user.js');
 const tokenvalidation = async (req,res,next) => {
     try {
         const authHeader = req.headers.authorization;
-        const token = authHeader && authHeader.split('')[1];
+        const token = authHeader && authHeader.split(' ')[1];
         if(!token){
             return res.status(401).json({message:"Invalid token", err:"No token provided"})
         }
