3010c45ae2bb2959d0dc2c01cbe1dc21b9370fa7
shivamgupta78 <shivamgupta2154@gmail.com>
2026-09-12 11:06:47 +0530

    Redis database updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter } from 'react-router'
-import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux';
 import {store} from './store/store.js'
