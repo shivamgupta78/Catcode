f34f804c072c53187cc661ab5cddc1a5f7114e61
shivamgupta78 <shivamgupta2154@gmail.com>
2026-09-12 13:09:14 +0530

    Tailwind Error resolved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter } from 'react-router'
 import App from './App.jsx'
 import { Provider } from 'react-redux';
 import {store} from './store/store.js'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
